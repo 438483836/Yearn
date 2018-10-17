10fe44e09af3c0953460f817773545c306707be8
liuyuang <liuya@ngarihealth.com>
2018-10-17 15:32:58 +0800

    阿里云短信接口优化

--- src/main/java/com/yearn/life/service/AliSmsServiceImpl.java
+++ src/main/java/com/yearn/life/service/AliSmsServiceImpl.java
@@ -1,32 +1,47 @@
 package com.yearn.life.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
 import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
-import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
+import com.google.common.collect.Maps;
+import com.yearn.life.utils.JSONUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
+@Component
+@Service("smsService")
 public class AliSmsServiceImpl {
+    //初始化ascClient需要的几个参数
+    //    @Value("${spring.alisms.product}")
+    final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
+    //    @Value("${spring.alisms.domain}")
+    final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
+//    @Value("${spring.web.accessKeyId}")
+    final String accessKeyId = "";//替换成你的AK
+//    @Value("${spring.web.accessKeySecret}")
+    final String accessKeySecret = "";//替换成你的accessKeySecret
 
-    public Map<String, Object> sendSms() throws Exception{
+    public Map<String, Object> sendSms(List<String> numberList, String signName, String templateCode, Map<String, String> templateMap){
+        Map<String, Object> result = Maps.newHashMap();
         try {
-            String phoneNumbers = "";
-            String signName = "";
-            String templateCode = "";
+            StringBuffer phoneNumbers = new StringBuffer(numberList.get(0));
+            for (String num : numberList) {
+                phoneNumbers = phoneNumbers.append(","+num);
+            }
+
+            String templateParam = JSONObject.toJSONString(templateMap);
             //设置超时时间-可自行调整
             System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
             System.setProperty("sun.net.client.defaultReadTimeout", "10000");
-            //初始化ascClient需要的几个参数
-            final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
-            final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
-            //替换成你的AK
-            final String accessKeyId = "yourAccessKeyId";//你的accessKeyId,参考本文档步骤2
-            final String accessKeySecret = "yourAccessKeySecret";//你的accessKeySecret，参考本文档步骤2
+
             //初始化ascClient,暂时不支持多region（请勿修改）
             IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                     accessKeySecret);
@@ -37,28 +52,31 @@ public class AliSmsServiceImpl {
             //使用post提交
             request.setMethod(MethodType.POST);
             //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式；发送国际/港澳台消息时，接收号码格式为国际区号+号码，如“85200000000”
-            request.setPhoneNumbers(phoneNumbers);
+            request.setPhoneNumbers(phoneNumbers.toString());
             //必填:短信签名-可在短信控制台中找到
             request.setSignName(signName);
             //必填:短信模板-可在短信控制台中找到，发送国际/港澳台消息时，请使用国际/港澳台短信模版
             request.setTemplateCode(templateCode);
             //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
             //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
-            request.setTemplateParam("{\"name\":\"Tom\", \"code\":\"123\"}");
+            request.setTemplateParam(templateParam);
             //可选-上行短信扩展码(扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段)
             //request.setSmsUpExtendCode("90997");
             //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
-            request.setOutId("yourOutId");
+//            request.setOutId("yourOutId12");
             //请求失败这里会抛ClientException异常
+            System.out.println("短信发送接口入参 request：" + JSONUtils.toString(request));
             SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
+            System.out.println("短信发送接口响应结果 =======> sendSmsResponse:" + JSONUtils.toString(sendSmsResponse));
             if(sendSmsResponse.getCode() != null && sendSmsResponse.getCode().equals("OK")) {
                 //请求成功
             }
-            return null;
-        } catch (ClientException e){
+            result.put("code", sendSmsResponse.getCode());
+            result.put("result", sendSmsResponse);
+            return result;
+        } catch (Exception e){
             e.printStackTrace();
             return null;
-
         }
 
     }
